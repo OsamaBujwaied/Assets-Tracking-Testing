2ddf619d3868add3b7b147c159e121b5f2b5460d
Yousef-Mush <67512409+Yousef-Mush@users.noreply.github.com>
2021-04-07 17:40:22 +0300

    Update models.py

--- assetstracking/models.py
+++ assetstracking/models.py
@@ -14,6 +14,7 @@ class Employee(models.Model):
     employee_id = models.IntegerField(null=True)
     employee_name = models.CharField(max_length=200, null=True)
     employee_salary = models.FloatField(null=True)
+    employeeX = models.IntegerField(null=True)
 
     subscriber_id = models.ManyToManyField(Subscriber)
 
