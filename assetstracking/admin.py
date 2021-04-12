02dc3dd64e6ca8ce1c796b48f12c1271c2472b47
khafcb <62855136+khafcb@users.noreply.github.com>
2021-04-12 19:44:57 +0300

    testingAD

--- assetstracking/admin.py
+++ assetstracking/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Subscriber, Employee, RFID, Tag, Borrowing, ClientAuth
 
 # Register your models here.
-
+'''
 admin.site.register(Subscriber)
 admin.site.register(Employee)
 admin.site.register(RFID)
@@ -13,25 +13,26 @@ admin.site.register(ClientAuth)
 
 
 '''
-admin.site.register(subscribers)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('subscriber_id', 'subscriber_name')
+admin.site.register(Subscriber, SubscriberAdmin)
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('employee_id', 'employee_name', 'employee_email', 'user')
-admin.site.register(Employees, EmployeeAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 
 class AssetsAdmin(admin.ModelAdmin):
-    list_display = ('tag_id', 'asset_name', 'asset_status', 'rfid_location')
+    list_display = ('tag_id', 'asset_name', 'asset_status', 'asset_location')
 admin.site.register(Tag, AssetsAdmin)
 
 class BorrowedAssetsAdmin(admin.ModelAdmin):
-    list_display = ('start_time0', 'end_time0', 'asset_id', 'employee_id', 'employee_id_scanned', 'asset_id_scanned')
-admin.site.register(BorrowedAssets, BorrowedAssetsAdmin)
+    list_display = ('borrowing_id', 'start_date', 'end_date', 'employee_id', 'tag_id', 'employee_id_scanned', 'asset_id_scanned', 'reader_code')
+admin.site.register(Borrowing, BorrowedAssetsAdmin)
 
 class ReadersAdmin(admin.ModelAdmin):
-    list_display = ('reader_id', 'reader_location')
-admin.site.register(Redaers, ReadersAdmin)
+    list_display = ('rfid_id', 'rfid_location')
+admin.site.register(RFID, ReadersAdmin)
 
 class ClientAuthAdmin(admin.ModelAdmin):
     list_display = ('client_username', 'client_password')
 admin.site.register(ClientAuth, ClientAuthAdmin)
-'''
