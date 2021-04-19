5fd1aadc05c15e3c6f33b2e4c48607612c70354e
Yousef-Mush <67512409+Yousef-Mush@users.noreply.github.com>
2021-04-19 11:17:21 +0300

    Update views.py

--- assetstracking/views.py
+++ assetstracking/views.py
@@ -136,7 +136,7 @@ def createBorrowing(request, pk):
         formset = BorrowingFormSet(request.POST, instance=employee)
         if formset.is_valid():
             formset.save()
-            return redirect('/subscriber/1')
+            return redirect('/employee')
 
     context = {'formset':formset}
     return render(request, 'assetstracking/createBorrowing.html', context)
